661c692eec29b2e9a00098e05fa9b0ca0b642f99
Robin Kerr <rnkerr@mtu.edu>
2019-10-26 15:01:29 -0400

    Player Movement Improvement
    Player moves by tiles now.

--- I am Trash/Assets/Scripts/PlayerControler.cs
+++ I am Trash/Assets/Scripts/PlayerControler.cs
@@ -2,18 +2,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerControler : MonoBehaviour
 {
     Rigidbody2D body;
 
-    float horizontal;
-    float vertical;
+    public float runSpeed = 3.0f;
+    public float moveCooldown = 0.2f;
 
-    public float runSpeed = 5.0f;
-    public float trashSlows = 0.0f;
+    public Tilemap groundMap;
+    public Tilemap wallsMap;
 
     private int trashBag = 0;
+    private bool isMoving = false;
+    private bool onCooldown = false;
+    private bool letsMove = false;
+
+    private Vector2 movement;
 
     void Start()
     {
@@ -25,20 +31,77 @@ public class PlayerControler : MonoBehaviour
 
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        if (isMoving || onCooldown) return;
+
+        Vector2 currentKeys = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (currentKeys != Vector2.zero)
+        {
+            movement = currentKeys;
+            letsMove = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        // Moves slower with more trash
-        float speed = runSpeed * (1 - (trashBag * trashSlows));
+        if (isMoving || onCooldown || !letsMove) return;
 
-        if (Mathf.Abs(horizontal) >= Mathf.Abs(vertical)) {
-            body.velocity = new Vector2(horizontal * speed, 0.0f);
-        } else {
-            body.velocity = new Vector2(0.0f, vertical * speed);
+        if (!movement.x.Equals(0.0f))
+        {
+            movement.y = 0.0f;
         }
+
+        StartCoroutine(MovementCooldown(moveCooldown));
+        Move(movement.x, movement.y);
+    }
+
+    private void Move(float xDir, float yDir)
+    {
+
+        Vector3 startPos = transform.position;
+        Vector3 endPos = new Vector3(startPos.x + xDir, startPos.y + yDir, startPos.z);
+
+        Vector3Int targetCell = new Vector3Int((int)(endPos.x - 0.5f), (int)(endPos.y - 0.5f), (int)endPos.z);
+
+        bool hasWall = wallsMap.GetTile(targetCell) != null;
+
+        if (!hasWall)
+        {
+            movement = Vector2.zero;
+            letsMove = false;
+
+            StartCoroutine(SmoothMove(startPos, endPos));
+        }
+    }
+
+    private IEnumerator SmoothMove(Vector3 startPos, Vector3 endPos)
+    {
+        isMoving = true;
+
+        float progress = 0.0f;
+
+        while (progress < 1f)
+        {
+            progress += Time.deltaTime * runSpeed;
+            transform.position = Vector3.Lerp(startPos, endPos, progress);
+
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+
+    private IEnumerator MovementCooldown(float cooldown)
+    {
+        onCooldown = true;
+
+        while (cooldown > 0f)
+        {
+            cooldown -= Time.deltaTime;
+            yield return null;
+        }
+
+        onCooldown = false;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
