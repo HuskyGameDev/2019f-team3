a1f2d79089d4ff2f8f8872cecd94d624de84eb88
Kevin Cornell <kdcornel@mtu.edu>
2019-10-26 14:44:03 -0400

    Added basic AI
    Added a basic AI sprite and path following script. It doesn't do anything but follow the path it has layed out.

---
+++ I am Trash/Assets/Kevin's Testing/Scripts/Patrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+    private int nextSpot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+        nextSpot = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[nextSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[nextSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                nextSpot++;
+                if ( nextSpot >= moveSpots.Length )
+                {
+                    nextSpot = 0;
+                }
+                waitTime = startWaitTime;
+            } else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
